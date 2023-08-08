03efbf81a6f0dcfb65cc43f53c52d0ec091295e8
Alpana Kushwaha <alpana95kush@gmail.com>
2023-08-08 16:25:56 +0530

    10th JS project added

---
+++ _10_MusicPlayer/script.js
@@ -0,0 +1,32 @@
+let progress = document.getElementById("progress-bar");
+let song = document.getElementById("song");
+let ctrlIcon = document.getElementById("ctrlIcon");
+
+song.onloadedmetadata = function () {
+  progress.max = song.duration;
+
+  progress.value = song.currentTime;
+};
+
+function playPause() {
+  if (ctrlIcon.classList.contains("fa-pause")) {
+    song.pause();
+    ctrlIcon.classList.remove("fa-pause");
+    ctrlIcon.classList.add("fa-play");
+  } else {
+    song.play();
+    ctrlIcon.classList.remove("fa-play");
+    ctrlIcon.classList.add("fa-pause");
+  }
+}
+if (song.play()) {
+  setInterval(() => {
+    progress.value = song.currentTime;
+  }, 500);
+}
+progress.onchange = function () {
+  song.play();
+  song.currentTime = progress.value;
+  ctrlIcon.classList.add("fa-pause");
+  ctrlIcon.classList.remove("fa-play");
+};
