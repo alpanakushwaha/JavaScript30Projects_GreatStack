68633aea535386d4db8ff272079a2c731dcfc2a0
Alpana Kushwaha <alpana95kush@gmail.com>
2023-08-09 11:08:36 +0530

    14th JS project HideAndShowPassword added

---
+++ _14_HideandShowPassword/script.js
@@ -0,0 +1,17 @@
+let eyeIcon = document.getElementById("eyeIcon");
+// document.getElementsByClassName('fa-eye-slash') will not work!!!!!
+
+let password = document.getElementById("password");
+
+eyeIcon.onclick = function () {
+  if (password.type == "password") {
+    password.type = "text";
+    eyeIcon.classList.remove("fa-eye-slash");
+    eyeIcon.classList.add("fa-eye");
+  } else {
+    password.type = "password";
+    eyeIcon.classList.remove("fa-eye");
+
+    eyeIcon.classList.add("fa-eye-slash");
+  }
+};
