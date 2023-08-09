64a3ba4d88d38f7551553f824b19880b3c5639de
Alpana Kushwaha <alpana95kush@gmail.com>
2023-08-09 18:32:47 +0530

    15th project updated, locally storing the selected dark/light theme

--- _15_DarkModeWebsite/script.js
+++ _15_DarkModeWebsite/script.js
@@ -1,12 +1,35 @@
 var icon = document.getElementById("icon");
+
+if (localStorage.getItem("theme") == null) {
+  localStorage.setItem("theme", "light");
+}
+
+let localData = localStorage.getItem("theme");
+
+if (localData == "light") {
+  icon.classList.remove("fa-moon");
+  icon.classList.add("fa-sun");
+  document.body.classList.remove("light-theme");
+  document.body.classList.add("dark-theme");
+} else if (localData == "dark") {
+  icon.classList.remove("fa-sun");
+  icon.classList.add("fa-moon");
+  document.body.classList.remove("dark-theme");
+  document.body.classList.add("light-theme");
+}
+
 icon.onclick = function () {
   document.body.classList.toggle("dark-theme");
   if (document.body.classList.contains("dark-theme")) {
     icon.classList.remove("fa-moon");
     icon.classList.add("fa-sun");
+
+    localStorage.setItem("theme", "dark");
+
     // icon.src=
   } else {
     icon.classList.remove("fa-sun");
     icon.classList.add("fa-moon");
+    localStorage.setItem("theme", "light");
   }
 };
