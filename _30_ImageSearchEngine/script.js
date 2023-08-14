97d8b4e4f60e64cd8433f63b9c52e46e74e23afc
Alpana Kushwaha <alpana95kush@gmail.com>
2023-08-14 18:29:15 +0530

    30th JS project ImageSearchEngine added

---
+++ _30_ImageSearchEngine/script.js
@@ -0,0 +1,47 @@
+const searchForm = document.getElementById("search-form");
+const searchBox = document.getElementById("search-box");
+const searchResult = document.getElementById("search-result");
+const showMoreBtn = document.getElementById("show-more-btn");
+
+const accessKey = "6XFzaUqufMuJy5QMHuoatGPdQP1HKqOHnPrPrwU0YyE";
+
+let keyword = "";
+let page = 1;
+
+async function searchImages() {
+  keyword = searchBox.value;
+  const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=12`;
+
+  const response = await fetch(url);
+  const data = await response.json();
+
+  if (page == 1) {
+    searchResult.innerHTML = "";
+  }
+
+  const results = data.results;
+
+  results.map((result) => {
+    const image = document.createElement("img");
+    image.src = result.urls.small;
+    const imageLink = document.createElement("a");
+    imageLink.href = result.links.html;
+    imageLink.target = "_blank";
+
+    imageLink.appendChild(image);
+    searchResult.appendChild(imageLink);
+  });
+
+  //   console.log(data);
+  showMoreBtn.style.display = "block";
+}
+searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  page = 1;
+  searchImages();
+});
+
+showMoreBtn.addEventListener("click", () => {
+  page++;
+  searchImages();
+});
