43e2fce2d9d363c3c153c74177e3380d6e978209
Alpana Kushwaha <alpana95kush@gmail.com>
2023-08-14 11:21:43 +0530

    27th project edited with neo inr value

--- _27_CryptoCurrencyWebsite/script.js
+++ _27_CryptoCurrencyWebsite/script.js
@@ -1,12 +1,12 @@
 var btc = document.getElementById("bitcoin");
-var neo = document.getElementById("neocoin");
+var neo = document.getElementById("neo");
 var lite = document.getElementById("litecoin");
 var eth = document.getElementById("ethereum");
 
 var settings = {
   async: true,
   crossDomain: true,
-  url: "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Cneo%2Clitecoin&vs_currencies=inr",
+  url: "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Clitecoin%2Cneo&vs_currencies=inr",
   method: "GET",
   headers: {},
 };
@@ -15,5 +15,5 @@ $.ajax(settings).done(function (response) {
   btc.innerHTML = response.bitcoin.inr;
   eth.innerHTML = response.ethereum.inr;
   lite.innerHTML = response.litecoin.inr;
-  neo.innerHTML = response.neocoin.inr;
+  neo.innerHTML = response.neo.inr;
 });
